210fa225ab7b1259008ec8e893a71d9fb71293c2
DESKTOP-UO1QQF5\hikme <hikmetozcann@outlook.com>
2024-02-12 00:31:27 +0300

    swagger setup

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ import express, { json, urlencoded  } from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
+import swaggerDocument from './swagger-output.json' assert { type: "json" };
+
+import swaggerUi from 'swagger-ui-express';
+
 import indexRouter from './routes/index.js';
 import NotFoundResponse from './utils/DefaultHttpResponses.js';
 import sequelize from './utils/database.js';
+
 sequelize.authenticate()
   .then(() => {
     console.log('Database connection established successfully.');   
@@ -15,9 +20,13 @@ sequelize.authenticate()
 var app = express();
 
 app.use(logger('dev'));
+
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 var router = Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
   return res.status(200).json({ message: 'Welcome to the API' });
 });
+
 
 export default router;
